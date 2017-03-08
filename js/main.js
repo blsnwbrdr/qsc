b4830ba9f262359444ea1f008424004a1d8799e7
Brandon Lew <brandon.t.lew@gmail.com>
2017-03-08 01:24:08 -0800

    added small animations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-$('.navbar-collapse ul li a').click(function() {
+$(document).ready(function() {
   "use strict";
-  $(this).closest('.collapse').collapse('toggle');
+  
+// CLOSE THE RESPONSIVE MENU ON CLICK
+  
+  $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
+  });
+  
+  // SCROLL REVEAL ANIMATION  
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.sr_blurb', {
+    duration: 1000,
+    delay: 300
+  });
+  sr.reveal('.sr_icon', {
+    duration: 500,
+    delay: 500,
+    distance: '0px',
+    scale: 0.2,
+  });
+
+  
 });
